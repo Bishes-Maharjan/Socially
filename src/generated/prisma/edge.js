fce831f7d8b473c7d2c3e08720be93107d22c294
Bishes Maharjan <maharjanbichheach0909@gmail.com>
2025-05-11 19:54:03 +0545

    package.json

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -214,11 +214,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://neondb_owner:npg_NE1vlhWLVF2D@ep-square-base-a4vx8hac-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require"
+        "value": null
       }
     }
   },
